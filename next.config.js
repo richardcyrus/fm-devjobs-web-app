c296178fcf81a8b001a53cd329f99509eb380554
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-07-14 11:08:35 -0400

    Setup Next.js app

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const securityHeaders = [
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'Referrer-Policy', value: 'same-origin' },
+]
+
+module.exports = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  eslint: {
+    dirs: ['pages', 'util', 'layouts', 'components', 'lib'],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Devjobs Web App</title>
+      </Head>
+      <h1>Hello, world!</h1>
+    </>
+  )
+}
