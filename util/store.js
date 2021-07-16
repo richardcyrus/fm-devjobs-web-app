c8e4094793a6cb11fd462fb88f37a3e210bfb682
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-07-16 15:26:51 -0400

    Change from magic strings alias for theme types

--- util/store.js
+++ util/store.js
@@ -5,6 +5,11 @@ export const ThemeContext = createContext({
   setTheme: () => {},
 })
 
+const themeTypes = {
+  light: 'light',
+  dark: 'dark',
+}
+
 export const ThemeContextProvider = ({ children }) => {
   const themes = {
     light: {
@@ -17,6 +22,7 @@ export const ThemeContextProvider = ({ children }) => {
       buttonSecondaryBackgroundColor: 'rgba(89, 100, 224, 0.1)',
       buttonSecondaryHoverColor: 'rgba(89, 100, 224, 0.35)',
       buttonTextColor: 'rgb(89, 100, 224)',
+      buttonPrimaryTextColor: 'rgb(255, 255, 255)',
       searchBarBackgroundColor: 'var(--color-secondary-white)',
       searchBarPlaceholderColor: 'rgba(25, 32, 45, 0.5)',
       searchBarLabelColor: 'var(--color-primary-very-dark-blue)',
@@ -35,6 +41,7 @@ export const ThemeContextProvider = ({ children }) => {
       buttonSecondaryBackgroundColor: 'rgba(255, 255, 255, 0.1)',
       buttonSecondaryHoverColor: 'rgba(255, 255, 255, 0.35)',
       buttonTextColor: 'rgb(255, 255, 255)',
+      buttonPrimaryTextColor: 'rgb(255, 255, 255)',
       searchBarBackgroundColor: 'var(--color-primary-very-dark-blue)',
       searchBarPlaceholderColor: 'rgba(255, 255, 255, 0.5)',
       searchBarLabelColor: 'var(--color-secondary-white)',
@@ -46,7 +53,10 @@ export const ThemeContextProvider = ({ children }) => {
   }
 
   const setTheme = (type) => {
-    setState({ ...state, theme: type === 'dark' ? themes.light : themes.dark })
+    setState({
+      ...state,
+      theme: type === themeTypes.dark ? themes.light : themes.dark,
+    })
   }
 
   const initialState = {
