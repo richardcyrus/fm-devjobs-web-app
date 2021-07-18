c25b57d1209c2cf2100839de77c45fbd69b7bed4
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-07-18 13:05:12 -0400

    Add React Query

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { ThemeContextProvider } from '../util/store'
 import '../styles/globals.scss'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <ThemeContextProvider>
-      {getLayout(<Component {...pageProps} />)}
+      <QueryClientProvider client={queryClient}>
+        {getLayout(<Component {...pageProps} />)}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeContextProvider>
   )
 }
